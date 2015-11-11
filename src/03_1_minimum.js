b81efe1614bf4a0b38ea51e9c1b099d856c612c5
Wojtek Ryrych <wojtek.ryrych@gmail.com>
2015-11-11 11:46:50 +0100

    Test for 03_1_minimum.js

---
+++ src/03_1_minimum.js
@@ -0,0 +1,3 @@
+export function myMin(a, b) {
+  return (a < b) ? a : b;
+}
---
+++ test/unit/03_1_minimum.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { myMin } from '../../src/03_1_minimum';
+
+describe('03_1:fizzbuzz', () => {
+
+  it('myMin is a function', () => {
+    expect(typeof myMin).to.equal('function');
+  });
+
+  it('returns minimum number', () => {
+    expect(myMin(0, 10)).to.equal(0);
+    expect(myMin(0, -10)).to.equal(-10);
+  });
+});
